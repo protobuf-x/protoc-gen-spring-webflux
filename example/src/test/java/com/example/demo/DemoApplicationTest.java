915ad9f4e915e6a07c250e59d94618211e97573a
disc99 <disc99.g@gmail.com>
2019-09-08 23:54:50 +0900

    Enable JUnit 5

--- example/src/test/java/com/example/demo/DemoApplicationTest.java
+++ example/src/test/java/com/example/demo/DemoApplicationTest.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTest {
 
@@ -15,7 +12,7 @@ public class DemoApplicationTest {
     private WebTestClient client;
 
     @Test
-    public void test() {
+    void test() {
         client.get().uri("/echo/1").exchange()
                 .expectBody()
                 .json("{\"echo\":{\"id\":10,\"content\":\"text1\"}}");
