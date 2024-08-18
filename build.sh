c6fa3c70cc9ab2cebc0c8293e455850ab1da14e6
disc99 <disc99.g@gmail.com>
2024-08-18 23:12:22 +0900

    Update

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ COMMAND=$1
 echo '>>> Init'
 rm -rf ~/.m2/repository/io/protobufx/protoc-gen-spring-webflux/local
 
+if [ "$COMMAND" = '-g' ]; then
+  echo '>>> Generate'
+  ./gradlew clean plugin:publishToMavenLocal example:generateProto
+fi
+
 if [ "$COMMAND" = '-t' ]; then
   echo '>>> Test'
   ./gradlew clean plugin:publishToMavenLocal example:generateProto example:test --stacktrace
@@ -13,4 +18,12 @@ fi
 if [ "$COMMAND" = '-r' ]; then
   echo '>>> Release'
   ./gradlew publish
+fi
+
+if [ "$COMMAND" = '-b' ]; then
+  echo '>>> buf'
+  ./gradlew clean plugin:publishToMavenLocal
+  cp plugin/build/libs/protoc-gen-spring-webflux-local.jar example/src/main/buf
+  cd example/src/main/buf
+  buf generate
 fi
